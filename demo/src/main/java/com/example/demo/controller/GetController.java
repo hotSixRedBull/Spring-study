b079d4add1229dfc2a75186440efb7c6b9970eb2
Alan Seunggi Lee <akkalee1207@gmail.com>
2021-11-24 21:03:48 +0900

    update api

--- demo/src/main/java/com/example/demo/controller/GetController.java
+++ demo/src/main/java/com/example/demo/controller/GetController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GetController {
@@ -14,9 +15,38 @@ public class GetController {
         return "hello";
     }
 
+    //MVC and template engine
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
        model.addAttribute("name", name);
        return "hello-template";
+    }
+
+    //API1, return plain text
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    //API2, return JSON object
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
