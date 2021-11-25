57d8d4a9bf96b2d42568890d8efe1494b4d409e0
Alan Seunggi Lee <akkalee1207@gmail.com>
2021-11-25 14:42:01 +0900

    Add member list view

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,10 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
+import com.example.demo.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private MemberService memberService;
 
     @GetMapping("/")
     public String home() {
